972812f36ebd3d69a273af815a5971ce6528d43d
Devi3east <devi3east@gmail.com>
2024-07-27 12:03:25 -0400

    Removed Mobile Application

--- site/src/jsMain/kotlin/com/example/swishersoftwaresolutions/components/LeftSide.kt
+++ site/src/jsMain/kotlin/com/example/swishersoftwaresolutions/components/LeftSide.kt
@@ -91,7 +91,7 @@ fun LeftSide(
             alwaysOpen = false,
             items = listOf(
                 AccordionItem(
-                    title = "Mobile Application & Software Development",
+                    title = "Software Development",
                     content = {
                         SpanText(
                             text = "Building robust and scalable applications tailored to your specific needs."
